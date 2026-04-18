88036f9244978a96ffd7e93bc003ed4f001e74a5
Artur <140997921+RutrO7@users.noreply.github.com>
2026-04-18 12:06:27 -0300

    Praticando

--- relember/app.js
+++ relember/app.js
@@ -1,1 +1,7 @@
-//Exercicios e praticas no node/js
+{
+  //Exercicios e praticas no node/js
+  function conta(a, b, c, d) {
+    return a + ((b * c) % d);
+  }
+  console.log(conta(35, 251, 37, 2));
+}
