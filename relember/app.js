fc30c054ad5d739a6def650e9bc70d7a71544175
Artur <140997921+RutrO7@users.noreply.github.com>
2026-04-21 02:42:07 -0300

    Criando commits

--- relember/app.js
+++ relember/app.js
@@ -5,3 +5,12 @@
   }
   console.log(conta(35, 251, 37, 2));
 }
+
+{
+  const hora = new Date();
+  console.log(
+    `hoje é dia ${hora.getDate()}, do mês ${hora.getMonth()}, do ano ${hora.getFullYear()}`,
+  );
+}
+let nome = artur;
+nome();
